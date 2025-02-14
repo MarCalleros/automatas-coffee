8c1b6e11c6f109331426d7f6b3b512a6f57bb958
Lokoloki <alejandrocalleros86@gmail.com>
2025-02-13 20:47:42 -0700

    Directorios actualizados y listos para deploy

--- public/index.php
+++ public/index.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
     <title>Document</title>
 </head>
 <body>
-    <?php include 'views/templates/header.php'; ?>
+    <?php include_once __Dir__ . "/../views/templates/header.php"; ?>
     
     <div>
         <h2>Nuestros Productos</h2>
 
-        <?php include 'views/carousel.php'; ?>
+        <?php include_once __Dir__ . "/../views/carousel.php"; ?>
     </div>
 
     <h2>Categorias mas Populares</h2>
@@ -20,7 +20,7 @@
     <div class="categories">
         <div class="category category--cafe-caliente">
             <div class="category__image">
-                <img src="/assets/img/category/cafe-caliente.png" alt="Categoria Frapuccinos">
+                <img src="/assets/img/category/cafe-caliente.png" alt="Categoria Café Caliente">
             </div>
             
             <div class="category__information">
@@ -32,7 +32,7 @@
 
         <div class="category category--cafe-frio">
             <div class="category__image">
-                <img src="/assets/img/category/cafe-frio.png" alt="Categoria Frapuccinos">
+                <img src="/assets/img/category/cafe-frio.png" alt="Categoria Café Frio">
             </div>
             
             <div class="category__information">
@@ -67,7 +67,7 @@
         </div>
     </div>
 
-    <?php include 'views/templates/footer.php'; ?>
+    <?php include_once __DIR__ . "/../views/templates/footer.php"; ?>
 
     <script src="assets/js/carouselX.js"></script>
     <script src="assets/js/navbar.js"></script>
