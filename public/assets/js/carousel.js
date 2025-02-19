2f072d0f35af108656febee2ecd3d77539b14ce5
Lokoloki <alejandrocalleros86@gmail.com>
2025-02-18 18:07:38 -0700

    Carrusel de libreria funcional y responsive

--- public/assets/js/carousel.js
+++ public/assets/js/carousel.js
@@ -1,78 +1,24 @@
-(function() {
-    let cards = document.querySelectorAll('.card');
-    const next = document.querySelector('.carousel__next-icon');
-    const prev = document.querySelector('.carousel__prev-icon');
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 60,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-    const ACTIVE_CARDS = 3;
-    const CARDS_SLIDE = 1;
-    const EXTRA_CARDS_EACH_SIDE = 2;
-    const REAL_CARDS = len - (EXTRA_CARDS_EACH_SIDE * 2);
-
-    let len = cards.length;
-    let i = EXTRA_CARDS_EACH_SIDE + 1;
-
-    next.addEventListener('click', function() {
-        if (i === len - EXTRA_CARDS_EACH_SIDE) {
-            cards.forEach(card => {
-                card.classList.remove('card--active');
-            });
-    
-            cards[EXTRA_CARDS_EACH_SIDE + 0].classList.add('card--active');
-            cards[EXTRA_CARDS_EACH_SIDE + 1].classList.add('card--active');
-            cards[EXTRA_CARDS_EACH_SIDE + 2].classList.add('card--active', 'slide-in-right');
-    
-            setTimeout(() => {
-                cards[EXTRA_CARDS_EACH_SIDE + 2].classList.remove('slide-in-right');
-            }, 500);
-        } else {
-            let nextIndex = (i + 2) % len;
-            let prevIndex = (i - 1 + len) % len; 
-    
-            cards[prevIndex].classList.remove('card--active');
-            cards[nextIndex].classList.add('card--active', 'slide-in-right');
-    
-            setTimeout(() => {
-                cards[nextIndex].classList.remove('slide-in-right');
-            }, 500);
+    breakpoints: {
+        768: {
+          slidesPerView: 2
+        },
+        1440: {
+          slidesPerView: 3
         }
-    
-        if (i === len - EXTRA_CARDS_EACH_SIDE) {
-            i = EXTRA_CARDS_EACH_SIDE + 1;
-        } else {
-            i++;
-        }
-    });
-    
-    prev.addEventListener('click', function() {
-        if (i === 1) {
-            cards.forEach(card => {
-                card.classList.remove('card--active');
-            });
-    
-            cards[len - EXTRA_CARDS_EACH_SIDE - 1].classList.add('card--active');
-            cards[len - EXTRA_CARDS_EACH_SIDE - 2].classList.add('card--active');
-            cards[len - EXTRA_CARDS_EACH_SIDE - 3].classList.add('card--active', 'slide-in-left');
-    
-            setTimeout(() => {
-                cards[len - EXTRA_CARDS_EACH_SIDE - 3].classList.remove('slide-in-left');
-            }, 500);
-        } else {
-            let nextIndex = (i + 1) % len;
-            let prevIndex = (i - 2 + len) % len; 
-    
-            cards[nextIndex].classList.remove('card--active');
-            cards[prevIndex].classList.add('card--active', 'slide-in-left');
-    
-            setTimeout(() => {
-                cards[prevIndex].classList.remove('slide-in-left');
-            }, 500);
-        };
-    
-        if (i === 1) {
-            i = len - EXTRA_CARDS_EACH_SIDE - 2;
-        } else {
-            i--;
-        }
-    });
-
-})();
+      }
+  });
--- views/carousel.php
+++ views/carousel.php
@@ -1,7 +1,7 @@
-<div class="carousel">
-            <img class="carousel__prev-icon" src="/assets/img/arrow-prev.png" alt="Anterior">
-
-            <div class="card">
+<div class="carousel swiper">
+    <div class="card-wrapper">
+        <ul class="card-list swiper-wrapper">
+            <li class="card swiper-slide">
                 <img class="card__image" src="/assets/img/product/cafe-helado.jpg" alt="Cafe Helado">
 
                 <div class="card__information">
@@ -13,16 +13,16 @@
                 <div class="card__footer">
                     <button class="card__footer-button">Agregar al carrito</button>
                     <div class="card__footer-like">
-                        <svg class="heart-icon" viewBox="0 0 24 24" width="26" height="26">
+                            <svg class="heart-icon" viewBox="0 0 24 24" width="26" height="26">
                             <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                         </svg>
                     </div>
                 </div>
-            </div>
+            </li>
 
-            <div class="card">
+            <li class="card swiper-slide">
                 <img class="card__image" src="/assets/img/product/capuccino.jpg" alt="Cafe Helado">
-                
+                        
                 <div class="card__information">
                     <h3 class="card__title">Capuccino</h3>
                     <p class="card__price">$70.00</p>
@@ -37,11 +37,11 @@
                         </svg>
                     </div>
                 </div>
-            </div>
+            </li>
 
-            <div class="card">
+            <li class="card swiper-slide">
                 <img class="card__image" src="/assets/img/product/espresso.jpg" alt="Cafe Helado">
-                
+                        
                 <div class="card__information">
                     <h3 class="card__title">Espresso</h3>
                     <p class="card__price">$65.00</p>
@@ -56,11 +56,11 @@
                         </svg>
                     </div>
                 </div>
-            </div>
+            </li>
 
-            <div class="card">
+            <li class="card swiper-slide">
                 <img class="card__image" src="/assets/img/product/chocolate-helado.jpg" alt="Cafe Helado">
-                
+                        
                 <div class="card__information">
                     <h3 class="card__title">Chocolate Helado</h3>
                     <p class="card__price">$75.00</p>
@@ -75,11 +75,11 @@
                         </svg>
                     </div>
                 </div>
-            </div>
+            </li>
 
-            <div class="card">
+            <li class="card swiper-slide">
                 <img class="card__image" src="/assets/img/product/machiato.jpg" alt="Cafe Helado">
-                
+                        
                 <div class="card__information">
                     <h3 class="card__title">Machiato</h3>
                     <p class="card__price">$85.00</p>
@@ -94,7 +94,13 @@
                         </svg>
                     </div>
                 </div>
-            </div>
+            </li>
+        </ul>
 
-            <img class="carousel__next-icon" src="/assets/img/arrow-next.png" alt="Siguiente">
-        </div>
+        
+    </div>
+        <div class="swiper-pagination"></div>
+
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+</div>
