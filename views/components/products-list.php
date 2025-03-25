98f9cac6597bb143589d7b7d3fba06e514fd0a99
Martin Alejandro Calleros Camarillo <alejandrocalleros86@gmail.com>
2025-03-25 11:42:18 -0700

    Ventana productos adaptada a tables y laptop la mitad

--- views/components/products-list.php
+++ views/components/products-list.php
@@ -10,16 +10,21 @@
                 <p class="product__price">$60.00</p>
                     <p class="product__description">Café helado con un toque de vainilla y caramelo</p>
             </div>
+        </div>
 
-            <div class="product__footer">
+        <div class="product__footer">
                 <button class="product__footer-button">Agregar al carrito</button>
+                <div class="product__footer-size">
+                    <div class="product__footer-size-option product__footer-size-option--small">C</div>
+                    <div class="product__footer-size-option product__footer-size-option--medium">M</div>
+                    <div class="product__footer-size-option product__footer-size-option--large">G</div>
+                </div>
                 <div class="product__footer-like">
                     <svg class="heart-icon" viewBox="0 0 24 24" width="26" height="26">
                         <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                     </svg>
                 </div>
             </div>
-        </div>
     </div>
 
     <div class="product">
@@ -33,16 +38,21 @@
                 <p class="product__price">$70.00</p>
                 <p class="product__description">Café espresso con leche vaporizada y una capa de espuma cremosa</p>
             </div>
+        </div>
 
-            <div class="product__footer">
+        <div class="product__footer">
                 <button class="product__footer-button">Agregar al carrito</button>
+                <div class="product__footer-size">
+                    <div class="product__footer-size-option product__footer-size-option--small">C</div>
+                    <div class="product__footer-size-option product__footer-size-option--medium">M</div>
+                    <div class="product__footer-size-option product__footer-size-option--large">G</div>
+                </div>
                 <div class="product__footer-like">
                     <svg class="heart-icon" viewBox="0 0 24 24" width="26" height="26">
                         <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                     </svg>
                 </div>
             </div>
-        </div>
     </div>
 
     <div class="product">
@@ -56,16 +66,21 @@
                 <p class="product__price">$65.00</p>
                 <p class="product__description">Café intenso y aromático con una crema dorada en la superficie</p>
             </div>
+        </div>
 
-            <div class="product__footer">
+        <div class="product__footer">
                 <button class="product__footer-button">Agregar al carrito</button>
+                <div class="product__footer-size">
+                    <div class="product__footer-size-option product__footer-size-option--small">C</div>
+                    <div class="product__footer-size-option product__footer-size-option--medium">M</div>
+                    <div class="product__footer-size-option product__footer-size-option--large">G</div>
+                </div>
                 <div class="product__footer-like">
                     <svg class="heart-icon" viewBox="0 0 24 24" width="26" height="26">
                         <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                     </svg>
                 </div>
             </div>
-        </div>
     </div>
 
     <div class="product">
@@ -79,16 +94,21 @@
                 <p class="product__price">$75.00</p>
                 <p class="product__description">Café intenso y aromático con una crema dorada en la superficie</p>
             </div>
+        </div>
 
-            <div class="product__footer">
+        <div class="product__footer">
                 <button class="product__footer-button">Agregar al carrito</button>
+                <div class="product__footer-size">
+                    <div class="product__footer-size-option product__footer-size-option--small">C</div>
+                    <div class="product__footer-size-option product__footer-size-option--medium">M</div>
+                    <div class="product__footer-size-option product__footer-size-option--large">G</div>
+                </div>
                 <div class="product__footer-like">
                     <svg class="heart-icon" viewBox="0 0 24 24" width="26" height="26">
                         <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                     </svg>
                 </div>
             </div>
-        </div>
     </div>
 
     <div class="product">
@@ -102,15 +122,20 @@
                 <p class="product__price">$85.00</p>
                 <p class="product__description">Café intenso y aromático con una crema dorada en la superficie</p>
             </div>
+        </div>
 
-            <div class="product__footer">
+        <div class="product__footer">
                 <button class="product__footer-button">Agregar al carrito</button>
+                <div class="product__footer-size">
+                    <div class="product__footer-size-option product__footer-size-option--small">C</div>
+                    <div class="product__footer-size-option product__footer-size-option--medium">M</div>
+                    <div class="product__footer-size-option product__footer-size-option--large">G</div>
+                </div>
                 <div class="product__footer-like">
                     <svg class="heart-icon" viewBox="0 0 24 24" width="26" height="26">
                         <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                     </svg>
                 </div>
             </div>
-        </div>
     </div>
 </div>
