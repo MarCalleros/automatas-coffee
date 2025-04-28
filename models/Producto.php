cbc3eedcf405c43f929a928098acbc1c3da020e6
AlexIA075 <163032623+irlepee@users.noreply.github.com>
2025-04-28 09:58:50 -0700

    Se cargan los elementos en el editproducto - Arreglar el checkbox, ta exótico

--- models/Producto.php
+++ models/Producto.php
@@ -374,17 +374,45 @@ class Producto {
 
     public static function findById($id) {
         require __DIR__ . '/../includes/database.php';
-        $query = "SELECT * FROM " . self::$tabla . " WHERE id = ? AND estatus = 1";
+    
+        // Datos básicos
+        $query = "SELECT * FROM producto WHERE id = ?";
         $stmt = mysqli_prepare($db, $query);
         mysqli_stmt_bind_param($stmt, 'i', $id);
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
-
-        if ($result->num_rows > 0) {
-            return new Producto(mysqli_fetch_assoc($result));
-        } else {
-            return null;
+        $producto = mysqli_fetch_assoc($result);
+    
+        if (!$producto) return null;
+    
+        // Categorías asociadas
+        $query = "SELECT id_categoria FROM producto_categoria WHERE id_producto = ?";
+        $stmt = mysqli_prepare($db, $query);
+        mysqli_stmt_bind_param($stmt, 'i', $id);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        $categorias = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $categorias[] = $row['id_categoria'];
         }
+        $producto['categorias'] = $categorias;
+    
+        // Tamaños y precios asociados
+        $query = "SELECT id_tamaño, precio FROM producto_tamaño WHERE id_producto = ?";
+        $stmt = mysqli_prepare($db, $query);
+        mysqli_stmt_bind_param($stmt, 'i', $id);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        $tamanos = [];
+        $precios = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $tamanos[] = $row['id_tamaño'];
+            $precios[$row['id_tamaño']] = $row['precio'];
+        }
+        $producto['tamanos'] = $tamanos;
+        $producto['precios'] = $precios;
+    
+        return $producto;
     }
 
     public static function create($nombre, $descripcion, $tamanos, $precios, $categorias, $rutaImagen) {
